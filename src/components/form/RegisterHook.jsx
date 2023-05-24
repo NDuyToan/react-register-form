c03654d4b2e3b37d15e96ac11ea068f84cd1384e
toan_nguyen <ndtoan@enjoyworks.co.kr>
2023-05-24 10:49:53 +0700

    148 thuc hanh register form voi rhf - custom input

--- src/components/form/RegisterHook.jsx
+++ src/components/form/RegisterHook.jsx
@@ -1,19 +1,44 @@
+import { useForm } from "react-hook-form";
+import InputHook from "../input/inputHook";
+
 const RegisterHook = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmitHandler = (value) => {
+    console.log("onSubmitHandler", value);
+  };
   return (
-    <div className="max-w-[300px] mx-auto my-10">
+    <form
+      onSubmit={handleSubmit(onSubmitHandler)}
+      className="max-w-[300px] mx-auto my-10"
+    >
       <div className="flex flex-col gap-3">
         <label htmlFor="username" className="cursor-pointer">
           Username
         </label>
-        <input
+        {/* <input
           type="text"
           id="username"
           placeholder="Enter your username"
           className="p-4 border border-gray-100 rounded-lg outline-none bg-white transition-all focus:border-blue-500"
-        />
+        /> */}
+        <InputHook
+          name="username"
+          placeholder="Enter your username"
+          id="username"
+          control={control}
+        ></InputHook>
         <p className="text-red-500 text-sm">Please enter you username </p>
       </div>
-    </div>
+      <button className="w-full bg-blue-500 text-white rounded-lg mt-5 font-semibold p-5">
+        Submit
+      </button>
+    </form>
   );
 };
 
