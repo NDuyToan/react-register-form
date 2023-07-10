64d0f8779f572dfe69d2ed0538b545f48971b4e5
toan_nguyen <ndtoan@enjoyworks.co.kr>
2023-07-10 10:28:55 +0700

    159 thuc hanh register form voi fomik - setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import RegisterHook from "./components/form/RegisterHook"
-
+import RegisterHook from "./components/form/RegisterHook";
+import RegisterFormik from "./components/form/RegisterFormik";
 
 function App() {
- 
-
   return (
     <div className="app">
-      <RegisterHook></RegisterHook>
+      {/* <RegisterHook></RegisterHook> */}
+      <RegisterFormik></RegisterFormik>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/input/inputFormik.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const InputFormik = () => {
+  return (
+    <input className="p-4 border border-gray-100 rounded-lg outline-none bg-white transition-all focus:border-blue-500" />
+  );
+};
+
+export default InputFormik;
